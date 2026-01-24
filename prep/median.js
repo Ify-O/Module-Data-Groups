83d50bbad2588317ff74bd41b657987ce4580a5b
Ifeoma <osegboifeoma3@gmail.com>
2026-01-24 10:40:49 +0100

    Add median calculation and logging for salary data

--- prep/median.js
+++ prep/median.js
@@ -4,3 +4,12 @@ function calculateMedian(list) {
 
   return median;
 }
+
+const salaries = [10, 20, 30, 40, 60, 80, 80];
+const median = calculateMedian(salaries);
+
+console.log(salaries, "<--- salaries input before we call calculateMean");
+const mean = calculateMean(salaries);
+
+console.log(`The median salary is ${median}`);
+console.log(`The mean salary is ${mean}`);
