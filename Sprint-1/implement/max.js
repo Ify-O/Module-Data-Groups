31d8666423e65f98e8fc5c7597cb1223117fd241
Ifeoma <osegboifeoma3@gmail.com>
2026-01-29 15:09:39 +0100

    created the test cases and refined them as I tested.

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,9 @@
-function findMax(elements) {
+function findMax(array) {
+  // filter only numbers and ignore others
+  const numbers = array.filter((item) => typeof item === "number");
+
+  // use the Math.max on filtered array
+  return numbers.length ? Math.max(...numbers) : -Infinity;
 }
 
 module.exports = findMax;
