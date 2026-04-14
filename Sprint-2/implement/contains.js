4d684f6e49b03675ae4b5f7c2b83683b3541567b
Ifeoma <osegboifeoma3@gmail.com>
2026-04-14 14:57:31 +0200

    feat: implement contains function to check property existence in objects

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,9 @@
-function contains() {}
+function contains(obj, prop) {
+  if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
+    return false;
+  }
+
+  return Object.hasOwn(obj, prop);
+}
 
 module.exports = contains;
