8a057b41260bc525e8706276b9011125b6d021f6
Ifeoma <osegboifeoma3@gmail.com>
2026-04-14 15:41:26 +0200

    Refactor tally test cases for clarity and completeness

--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -19,16 +19,32 @@ const tally = require("./tally.js");
 // Given a function called tally
 // When passed an array of items
 // Then it should return an object containing the count for each unique item
+test("counts frequency of items in an array", () => {
+  expect(tally(["a", "a", "b", "c"])).toEqual({
+    a: 2,
+    b: 1,
+    c: 1,
+  });
+});
 
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
-test.todo("tally on an empty array returns an empty object");
+test("tally on an empty array returns an empty object", () => {
+  expect(tally([])).toEqual({});
+});
 
 // Given an array with duplicate items
 // When passed to tally
 // Then it should return counts for each unique item
-
+test("counts repeated items correctly", () => {
+  expect(tally(["a", "a", "a"])).toEqual({
+    a: 3,
+  });
+});
 // Given an invalid input like a string
 // When passed to tally
 // Then it should throw an error
+test("throws error for invalid input", () => {
+  expect(() => tally("abc")).toThrow("Input must be an array");
+});
